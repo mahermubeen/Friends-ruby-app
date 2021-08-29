c307a95afc496625e06220ee93c45d4179aba65a
mahermubeen <mahermubeenahmad@gmail.com>
2021-08-29 07:52:51 +0500

    heehe

--- config/application.rb
+++ config/application.rb
@@ -3,6 +3,7 @@ require_relative "boot"
 require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
+config.assets.initialize_on_precompile = false
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
